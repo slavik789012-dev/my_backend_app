2727b45274031fcea0e2a77299def7c046ce2568
Slavik <slavik789012@gmail.com>
2026-09-15 13:11:36 +0400

    feat: добавлено выделение памяти

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 int main() {
-	std::cout << "Конфликт успешно разрешен!" << std::endl;
-	return 0;
+    std::cout << "Конфликт успешно разрешен!" << std::endl;
+    int* bad_ptr = new int[1000000];
+    return 0;
 }
